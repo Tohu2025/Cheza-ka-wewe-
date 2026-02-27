dfdf977c1fa6f88d10cb0f522585053d3f710ef7
Tohu2025 <tohutarus001@gmail.com>
2026-02-28 00:32:34 +0300

    Update Logo.tsx

--- Components/Logo.tsx
+++ Components/Logo.tsx
@@ -1,1 +1,14 @@
-components
+"use client";
+
+import { Coffee } from "lucide-react";
+
+export default function Logo() {
+  return (
+    <div className="flex items-center gap-3">
+      <div className="bg-orange-600 p-3 rounded-2xl">
+        <Coffee size={28} className="text-white" />
+      </div>
+      <span className="text-2xl font-bold">Cheza Kama Wewe</span>
+    </div>
+  );
+}
